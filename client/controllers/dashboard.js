ee0194c918c99602b0c03e99017a3bc47ce77a3b
Juan Apolinar <juanapolinar@my.unt.edu>
2015-03-23 13:50:27 -0500

    undo

--- client/controllers/dashboard.js
+++ client/controllers/dashboard.js
@@ -2,8 +2,6 @@ Template.createProduct.events({
   'submit': function(event) {
     var product = {
       name: event.target.productName.value.trim(),
-      inVisible: true
-      name: event.target.shortDescription.value.trim(),
       isVisible: true
     };
 
