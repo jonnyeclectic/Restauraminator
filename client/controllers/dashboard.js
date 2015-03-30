374681dafc20535604385435630c4e547c9f77da
Juan Apolinar <juanapolinar@my.unt.edu>
2015-03-30 14:08:33 -0500

    change

--- client/controllers/dashboard.js
+++ client/controllers/dashboard.js
@@ -2,6 +2,7 @@ Template.createProduct.events({
   'submit': function(event) {
     var product = {
       name: event.target.productName.value.trim(),
+      shortDescription: event.target.productshortDescription.value.trim(),
       isVisible: true
     };
 
