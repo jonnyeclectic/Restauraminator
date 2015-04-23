6704e32a7e0f32bd71bfeff45b80a52a4df26777
Jake Singer <jake@jakesinger.com>
2015-04-22 21:51:23 -0500

    The order status can now be changed. A new view that lists the orders and their status is also now available

--- common/collections/orders.js
+++ common/collections/orders.js
@@ -1,3 +1,9 @@
+// All the values status can have
+var statusValues = [
+  "processing",
+  "complete"
+];
+
 // Schema
 Schemas.Orders = new SimpleSchema({
   _id: {
@@ -9,6 +15,12 @@ Schemas.Orders = new SimpleSchema({
   },
   timestamp: {
     type: Date
+  },
+  userId: {
+    type: String
+  },
+  status: {
+    type: String
   }
 });
 
@@ -22,7 +34,27 @@ Meteor.methods({
     var storeId = Collections.Stores.findOne({ owner: Meteor.userId() })._id;
     Collections.Orders.insert({
       storeId: storeId,
-      timestamp: options.timestamp
+      timestamp: options.timestamp,
+      userId: Meteor.userId(),
+      status: statusValues[0]
     });
+  },
+
+  processOrder: function(_id) {
+    var order = Collections.Orders.findOne({ _id: _id });
+    if (!order)
+      return;
+
+    var statusIndex = statusValues.indexOf(order.status) + 1;
+    if (statusIndex >= statusValues.length)
+      statusIndex = 0;
+
+    Collections.Orders.update({
+      _id: _id
+    },{
+      $set: {
+        status: statusValues[statusIndex]
+      }
+    },{});
   }
 });
